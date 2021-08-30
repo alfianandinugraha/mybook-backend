320d9bcafdcd30c0ce13f138d8d0c059915560c5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-30 23:52:35 +0700

    chore(flag): add --files on ts-node exec

--- services/user.ts
+++ services/user.ts
@@ -1,8 +1,18 @@
 import Low from "lowdb"
 import FileSync from "lowdb/adapters/FileSync"
 import { join } from "path"
-import { UserDB } from "types"
+import { User, UserDB } from "types"
 
 const file = join(__dirname, "..", "db", "users.json")
 const adapter = new FileSync<UserDB>(file)
 const db = Low(adapter)
+
+const register = (user: User) => {
+	db.get("users").push(user)
+}
+
+const User = {
+	register,
+}
+
+export default User
