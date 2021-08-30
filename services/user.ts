a2b15f00546e9782bbc151c23fa63c9cb7f01fe3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-31 00:08:32 +0700

    feat(methods): add findEmail and register user

--- services/user.ts
+++ services/user.ts
@@ -1,18 +1,28 @@
 import Low from "lowdb"
 import FileSync from "lowdb/adapters/FileSync"
 import { join } from "path"
+import bcrypt from "bcrypt"
 import { User, UserDB } from "types"
 
 const file = join(__dirname, "..", "db", "users.json")
 const adapter = new FileSync<UserDB>(file)
 const db = Low(adapter)
 
+const findEmail = (email: string): User | undefined => {
+	return db.get("users").find({ email }).value()
+}
+
 const register = (user: User) => {
-	db.get("users").push(user)
+	const newUser = {
+		...user,
+		password: bcrypt.hashSync(user.password, bcrypt.genSaltSync(5)),
+	}
+	return db.get("users").push(newUser).write()
 }
 
-const User = {
+const UserService = {
 	register,
+	findEmail,
 }
 
-export default User
+export default UserService
