bde51dcb56058575789a338319a5bc56581907f3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-31 00:32:36 +0700

    feat(services): add token services to generate jwt

---
+++ services/token.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken"
+import { Token } from "types"
+
+const { ACCESS_TOKEN_SECRET_KEY, REFRESH_TOKEN_SECRET_KEY } = process.env
+
+function generateAccessToken<T extends {}>(payload: T) {
+	return jwt.sign(payload, ACCESS_TOKEN_SECRET_KEY as string, { expiresIn: "5m" })
+}
+
+function generateRefreshToken<T extends {}>(payload: T) {
+	return jwt.sign(payload, REFRESH_TOKEN_SECRET_KEY as string, { expiresIn: "30d" })
+}
+
+function generateToken<T extends {}>(payload: T): Token {
+	const accessToken = generateAccessToken(payload)
+	const refreshToken = generateRefreshToken(payload)
+
+	return {
+		accessToken,
+		refreshToken,
+	}
+}
+
+const TokenService = {
+	getAll: generateToken,
+	getAccess: generateAccessToken,
+	getRefresh: generateRefreshToken,
+}
+
+export default TokenService
+export { generateToken }
