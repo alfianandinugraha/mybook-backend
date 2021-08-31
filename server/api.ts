9b6ad171d0c16e8657062416240e8bddf12fd3f8
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-31 13:05:00 +0700

    feat(endpoint): add /profile endpoint

--- server/api.ts
+++ server/api.ts
@@ -1,10 +1,19 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import verifyToken from "@/middlewares/verifyToken"
+import { ApiResponse, UserLocals, UserProps } from "types"
 
 const server = express()
 const router = express.Router()
 
 router.get("/", (_, res) => {
 	return res.json("Hello world !")
+})
+
+router.get("/profile", verifyToken, (req: Request, res: Response<ApiResponse<UserProps>, UserLocals>) => {
+	return res.json({
+		message: "Success get user profile",
+		data: res.locals.user,
+	})
 })
 
 server.use("/api", router)
