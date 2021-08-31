a95077cb167ffdcf7fe57eed59f4dbcc0f30de61
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-31 14:58:32 +0700

    feat(endpoint): add endpoint to add book

--- server/api.ts
+++ server/api.ts
@@ -1,10 +1,32 @@
 import express, { Request, Response } from "express"
 import verifyToken from "@/middlewares/verifyToken"
-import { ApiResponse, UserLocals, UserProps } from "types"
+import Ajv from "ajv"
+import { ApiResponse, Book, UserLocals, UserProps } from "types"
+import { v4 } from "uuid"
+import BookService from "@/services/book"
 
 const server = express()
 const router = express.Router()
+const ajv = new Ajv()
 
+const requestBookSchema = {
+	type: "object",
+	properties: {
+		title: {
+			type: "string",
+		},
+		authors: {
+			type: "array",
+		},
+		description: {
+			type: "string",
+		},
+	},
+	required: ["title", "authors", "description"],
+	additionalProperties: false,
+}
+
+router.use(express.json())
 router.get("/", (_, res) => {
 	return res.json("Hello world !")
 })
@@ -14,6 +36,35 @@ router.get("/profile", verifyToken, (req: Request, res: Response<ApiResponse<Use
 		message: "Success get user profile",
 		data: res.locals.user,
 	})
+})
+
+router.post("/books", verifyToken, (req: Request, res: Response<ApiResponse<{}>, UserLocals>) => {
+	const isValidBody = ajv.validate(requestBookSchema, req.body)
+	if (!isValidBody) return res.status(400).json({ message: "Invalid body", data: {} })
+
+	const bookId = v4()
+	const { title, description, authors } = req.body
+	const book: Book = {
+		id: bookId,
+		userId: res.locals.user.id,
+		title,
+		description,
+		authors,
+	}
+
+	try {
+		BookService.store(book)
+		return res.json({
+			message: "Success store book",
+			data: book,
+		})
+	} catch (err) {
+		console.log(err)
+		return res.status(400).json({
+			message: "Failed to store book",
+			data: {},
+		})
+	}
 })
 
 server.use("/api", router)
